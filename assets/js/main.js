a95ee3f42c60b247147a3c6a901d66bff4839c36
MichaelGee76 <gottungmichael@gmail.com>
2024-02-11 18:46:50 +0100

    all functions are working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
+// Erstellt eine neues array
 let newMovies = movies;
 
 const outputContainer = document.querySelector(".output");
 const bestErrors = document.querySelector(".bestErrors");
 const noMovies4You = document.querySelector(".noMovies4You");
-console.log(bestErrors);
 
 // Der Code erstellt Kacheln je nach Anzahl der Filme in Array. Das Array im Array wir auch per map angesprochen und mit join() wir der br tag hinzugefügt
 
-const output = movies.map((tile) => {
+const output = newMovies.map((tile) => {
     outputContainer.innerHTML += `<div class="tile">
     <h2>${tile[0]}</h2>
     <h4>${tile[1]}</h4>
@@ -33,37 +33,13 @@ function showMovies(array) {
     });
 }
 
-// Das ist die Suchfunktion, erstellt ein Array und baut mit showMovies() das html auf
-// Aktuell noch am testen, funktioniert noch nicht
-// function findMovie() {
-//     const searchBarValue = document.querySelector(".searchBar").value.toLowerCase();
-//     if (newMovies.length === 0) {
-//         newMovies = movies.filter((result) => {
-//             return (
-//                 result[0].toLowerCase().includes(searchBarValue) || result[1].includes(searchBarValue) || result[2].toLowerCase().includes(searchBarValue) || result[3].toLowerCase().includes(searchBarValue) || result[4].join().toLowerCase().includes(searchBarValue) || result[5] === searchBarValue
-//             );
-//         });
-//         outputContainer.innerHTML = "";
-//         showMovies(newMovies);
-//     } else {
-//         bestErrors.classList.add("spanRed");
-//         noMovies4You.classList.add("spanRed");
-//         noMovies4You.textContent = "NO Movies4You";
-//         bestErrors.innerHTML = `Movie Not found`;
-//         setTimeout(() => {
-//             noMovies4You.classList.remove("spanRed");
-//             noMovies4You.textContent = "Movies4You";
-//             bestErrors.classList.remove("spanRed");
-//             bestErrors.innerHTML = "The best MovieDB";
-//         }, 3500);
-//     }
-// }
-
 function findMovie() {
     const searchBarValue = document.querySelector(".searchBar").value.toLowerCase();
     newMovies = movies.filter((result) => {
         return result[0].toLowerCase().includes(searchBarValue) || result[1].includes(searchBarValue) || result[2].toLowerCase().includes(searchBarValue) || result[3].toLowerCase().includes(searchBarValue) || result[4].join().toLowerCase().includes(searchBarValue) || result[5] === searchBarValue;
     });
+    const count = document.querySelector("#count");
+    count.innerHTML = `${newMovies.length} movies match your criteria`;
     errorMsg();
     outputContainer.innerHTML = "";
     showMovies(newMovies);
@@ -77,25 +53,25 @@ function errorMsg() {
         bestErrors.innerHTML = `Movie Not found`;
         setTimeout(() => {
             noMovies4You.classList.remove("spanRed");
-            noMovies4You.textContent = "Movies4You";
+            noMovies4You.innerHTML = "Movies4You";
             bestErrors.classList.remove("spanRed");
             bestErrors.innerHTML = "The best MovieDB";
         }, 3500);
         // outputContainer.innerHTML = "";
         // window.location.reload();
-        showMovies(newMovies);
+        showMovies(movies);
     } else {
         return;
     }
 }
-
-// funktion geht, teste gerade die obere version
 
 // Fragt per eventlistener ab ob geclickt ? function : nix :)
 
 function sort() {
     const up = document.querySelector("#up").addEventListener("click", function () {
         newMovies.sort((a, b) => {
+            console.log("worked up");
+
             return a[1] - b[1];
         });
         outputContainer.innerHTML = "";
@@ -103,6 +79,8 @@ function sort() {
     });
     const down = document.querySelector("#down").addEventListener("click", function () {
         newMovies.sort((a, b) => {
+            console.log("worked down");
+
             return b[1] - a[1];
         });
         outputContainer.innerHTML = "";
@@ -116,5 +94,3 @@ function sort() {
         showMovies(newMovies);
     });
 }
-
-console.log(movies.join().includes("The"));
